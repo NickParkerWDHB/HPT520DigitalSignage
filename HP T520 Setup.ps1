03deeb340debbd4cad88ae82d5e727bd89bfad52
NickParkerWDHB <94996203+NickParkerWDHB@users.noreply.github.com>
2022-07-30 20:51:51 +1200

    Update HP T520 Setup.ps1

--- HP T520 Setup.ps1
+++ HP T520 Setup.ps1
@@ -9,12 +9,18 @@ Set-Itemproperty -path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlo
 Set-Itemproperty -path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name 'DefaultPassword' -value 'Administrator'
 New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DisableLockWorkstation" -Value "1"  -PropertyType "DWord"
 
-Get-WmiObject -Class Win32_Product | Select-Object -Property Name
+$PowerPlan = Get-WmiObject -Namespace root\cimv2\power -Class Win32_PowerPlan -Filter "ElementName = 'Balanced'"
+$PowerPlan.Activate()
 
-$Apps = @('HP Easy Shell','HP Thin Update','HP Write Manager','Broadcom Bluetooth Software','Foxit PhantomPDF','HP System Information','Intel(R) PRO/Wireless Driver')
+powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
+powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 96996bc0-ad50-47ec-923b-6f41874dd9eb 0
+powercfg /SETACVALUEINDEX SCHEME_CURRENT SUB_NONE CONSOLELOCK 0
+
+$Apps = @('HP Easy Shell', 'HP System Information', 'Foxit PhantomPDF', 'HP ThinUpdate', 'VMware Horizon Client')
 
 foreach ($app in $Apps)
 	{
+		Write-Output("Uninstalling: " + $app)
 		$MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq $app}
 		$MyApp.Uninstall()
 	}
