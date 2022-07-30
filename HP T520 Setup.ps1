0341ba48c46cda43c5f070b7e6c62161ce4992ee
NickParkerWDHB <94996203+NickParkerWDHB@users.noreply.github.com>
2022-07-30 19:38:50 +1200

    Update HP T520 Setup.ps1

--- HP T520 Setup.ps1
+++ HP T520 Setup.ps1
@@ -2,6 +2,8 @@ Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
 
 Clear-Host
 
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\Current Version\Winlogony" -Name "NetwrixParam" -Value ”NetwrixValue”  -PropertyType "String"
+
 Get-WmiObject -Class Win32_Product | Select-Object -Property Name
 
 $Apps = @('HP Easy Shell','HP Thin Update','HP Write Manager','Broadcom Bluetooth Software','Foxit PhantomPDF','HP System Information','Intel(R) PRO/Wireless Driver')
